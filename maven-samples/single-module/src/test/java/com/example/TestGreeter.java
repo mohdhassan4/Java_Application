22c81804a94d224aa4d4fa5814630d969577d163
mohdhassan4 <64826151+mohdhassan4@users.noreply.github.com>
2024-11-02 13:57:58 +0530

    Failed case commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,6 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+
+   @Test
+  public void IntensionallyFailed() {
+    assertTrue(false);
+  }
   
 
   @Test
